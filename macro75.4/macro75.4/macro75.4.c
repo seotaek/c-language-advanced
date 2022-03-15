11a3049c81a412dd706d14557c674e65d055a07f
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-15 15:40:57 +0900

    코딩도장 75.4스왑 매크로

---
+++ macro75.4/macro75.4/macro75.4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#define swap(a,b,type) do{\
+	type temp;\
+	temp=a;\
+	a=b;\
+	b=temp;\
+}while(0)//a와b의 값을 서로 바꿈
+
+int main() {
+	int num1 = 10;
+	int num2 = 20;
+
+	swap(num1, num2, int);
+	printf("%d %d\n", num1, num2);
+
+	float num3 = 1.5f;
+	float num4 = 3.8f;
+
+	swap(num3, num4, float);
+	printf("%f %f\n", num3, num4);
+
+	return 0;
+}
