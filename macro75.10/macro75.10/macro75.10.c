bef11b57ea3a91ddaa8dd3d4be65da00be5ebf55
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-15 16:22:33 +0900

    코딩도장75.10 심사문제

---
+++ macro75.10/macro75.10/macro75.10.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define RGB(r,g,b) (((unsigned char)(r))|((unsigned short) ((unsigned char)(g)<<8))|((unsigned int)(unsigned char)(b)<<16))
+
+int main()
+{
+    int r, g, b;
+    scanf("%d %d %d", &r, &g, &b);
+
+    printf("#%06X\n", RGB(r, g, b));
+    printf("#%06X\n", RGB(1 & 1, 2 & 2, 3 & 3));
+
+    return 0;
+
+}
